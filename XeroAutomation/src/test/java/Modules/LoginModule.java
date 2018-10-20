f3ca7fb2cf8f98505fbf8a270d08432547b9d2f6
som <som@som-PC>
2018-10-20 15:53:56 -0700

    Initial commit_xero

---
+++ XeroAutomation/src/test/java/Modules/LoginModule.java
@@ -0,0 +1,32 @@
+package Modules ;
+
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+
+
+public class LoginModule {
+	
+//	public  WebDriver loginToXero(WebDriver driver){
+//		WebElement username= driver.findElement(By.xpath("//input[@id='email']"));		
+//		username.sendKeys("swati.wani@gmail.com");
+//		WebElement password=driver.findElement(By.xpath("//input[@id='password']"));
+//		password.sendKeys("shrinidhi1");
+//		WebElement loginBTN= driver.findElement(By.xpath("//button[@id='submitButton']"));
+//		loginBTN.click();
+//
+//		return driver;
+//		
+//	}
+			
+	
+}	
+	
+	
